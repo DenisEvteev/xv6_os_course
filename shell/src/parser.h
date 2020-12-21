56b8a453b77eac7e125296f5fc8a32ed8939cb5e
eda_3802 <evteev.da@phystech.edu>
2020-12-21 21:35:56 +0300

    add brackets; list of commands; background state

---
+++ shell/src/parser.h
@@ -0,0 +1,19 @@
+
+
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include "run.h"
+#include "processing.h"
+
+struct cmd *get_pipe(char **, char *);
+struct cmd *get_parentheses(char **, char *);
+struct cmd *get_exec(char **, char *);
+struct cmd* get_redirection(struct cmd *cmd, char **ps, char *es);
+
+struct cmd* create_pipecmd(struct cmd *left, struct cmd *right);
+struct cmd* create_redir(struct cmd *subcmd, char *file, int type);
+struct cmd* cur_command(struct cmd *current);
+struct cmd * change_redirection_in_sequence(char **ps, const char *es, struct cmd *cmd, int tok);
+
+#endif //_PARSER_H_
---
+++ shell/src/processing.h
@@ -0,0 +1,13 @@
+#ifndef _PROCESSING_H_
+#define _PROCESSING_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int get_token(char **ps, const char *es, char **q, char **eq);
+char *copy_token(char *s, const char *es);
+int peek(char **ps, const char *es, char *toks);
+
+#endif //_PROCESSING_H_
